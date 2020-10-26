12c54326559293f123b5a92014288f9ea61aad2b
Kayla Bullard <kaylabullard79@gmail.com>
2020-10-26 01:33:06 -0400

    Table Push

--- src/Music/MusicTable.tsx
+++ src/Music/MusicTable.tsx
@@ -7,7 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Button, TablePagination, TextField } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 // import { TrackList } from './MusicInterface';
 
